9b7c2ed028e75df30e7cd927eb6802e511672c42
Franco Gaytan <gaytanfranco@gmail.com>
2022-04-25 18:21:28 -0300

    CommitCambioFormato

--- node-rest-recipes/models/user.js
+++ node-rest-recipes/models/user.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 
-const usersSchema =  mongoose.Schema({
-    userName: {
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true
-    }
-})
+const usersSchema = mongoose.Schema({
+	userName: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	}
+});
 
-export default mongoose.model("User", usersSchema)
+export default mongoose.model('User', usersSchema);
--- node-rest-recipes/services/ingredients.service.js
+++ node-rest-recipes/services/ingredients.service.js
@@ -1,36 +1,31 @@
-import MongoBaseService from "../services/mongo.service.js";
+import MongoBaseService from '../services/mongo.service.js';
 
-import Ingredient from "../models/ingredient.js";
-
-
+import Ingredient from '../models/ingredient.js';
 
 export default class IngredientsService {
+	#mongoBaseService;
 
-    #mongoBaseService;
+	constructor() {
+		this.#mongoBaseService = new MongoBaseService(Ingredient);
+	}
 
-    constructor() {
-        this.#mongoBaseService = new MongoBaseService(Ingredient);
-    }
+	async getMany(req, res) {
+		return await this.#mongoBaseService.getMany(req, res);
+	}
 
-    async getMany(req, res) {
-        return await this.#mongoBaseService.getMany(req, res);
-    }
+	async getOne(req, res) {
+		return await this.#mongoBaseService.getOne(req, res);
+	}
 
-    async getOne(req, res) {
-        return await this.#mongoBaseService.getOne(req, res);
-    }
+	async create(req, res) {
+		return await this.#mongoBaseService.create(req, res);
+	}
 
-    async create(req, res) {
-        return await this.#mongoBaseService.create(req, res);
-    }
+	async update(req, res) {
+		return await this.#mongoBaseService.update(req, res);
+	}
 
-    async update(req, res) {
-        return await this.#mongoBaseService.update(req, res);
-    }
-
-    async delete(req, res) {
-        return await this.#mongoBaseService.delete(req, res);
-    }
-
+	async delete(req, res) {
+		return await this.#mongoBaseService.delete(req, res);
+	}
 }
-
