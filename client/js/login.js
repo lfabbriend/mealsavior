6b62f21a905b4d8e1ae570ea2c842f0e03c22feb
lfabbriend <lucia.fabbri@endava.com>
2022-04-26 17:16:32 -0300

    Form date succesfully taken and sent to the API

---
+++ client/js/login.js
@@ -0,0 +1,26 @@
+import { baseApiUrl } from './utils/constants.js';
+
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	const formData = new FormData(event.currentTarget);
+	const userName = formData.get('userName');
+	const email = formData.get('email');
+	const password = formData.get('password');
+	fetch(`${baseApiUrl}/login`, {
+		method: 'POST',
+		body: JSON.stringify({
+			userName,
+			email,
+			password
+		}),
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		}
+	})
+		.then(response => response.json())
+		.then(json => console.log(json))
+		.catch(error => console.warn(error));
+});
