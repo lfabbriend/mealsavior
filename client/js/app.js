dacc4e229a89d8678803114963bae60ac47c556b
Franco Gaytan <gaytanfranco@gmail.com>
2022-04-27 11:52:42 -0300

    new rater

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,10 @@
-import "./login.js";
+import { Rater } from './rater.js';
+
+const btn = document.getElementById("logInBtn");
+
+document.addEventListener('DOMContentLoaded', function(){
+	const raters = document.querySelectorAll('[role=rater]');
+	raters.forEach(rater=>{
+		new Rater(rater);
+	});
+});
