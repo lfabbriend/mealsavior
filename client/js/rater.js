825d74a54626f25731edf31ca3cab3cf991d854e
lfabbriend <lucia.fabbri@endava.com>
2022-05-06 13:38:46 -0300

    CSS recipe page changed

--- client/js/rater.js
+++ client/js/rater.js
@@ -1,27 +1,25 @@
-export function Rater(ratingElement){
-
+export function Rater(ratingElement) {
 	const stars = ratingElement.querySelectorAll('.star');
 
-	const setRating = ev =>{
-		ratingElement.setAttribute('data-rating', ev.currentTarget.getAttribute('data-value')
-		);
+	const setRating = ev => {
+		ratingElement.setAttribute('data-rating', ev.currentTarget.getAttribute('data-value'));
 	};
 
-	const resetRating = ev =>{
+	const resetRating = ev => {
 		const currentRating = ratingElement.getAttribute('data-rating');
 		highlightRating(currentRating);
-	}
+	};
 
-	const highlightRating = (rating)=>{
-		stars.forEach(star=>{
-			star.style.color = rating >= star.getAttribute('data-value') ? 'SeaGreen' : 'gray';
+	const highlightRating = rating => {
+		stars.forEach(star => {
+			star.style.color = rating >= star.getAttribute('data-value') ? 'Yellow' : 'White';
 		});
-	}
-	
+	};
+
 	resetRating();
 
-	stars.forEach(star=>{
-		star.addEventListener('click', setRating)
+	stars.forEach(star => {
+		star.addEventListener('click', setRating);
 	});
 
 	ratingElement.addEventListener('mouseout', resetRating);
