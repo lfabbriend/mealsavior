3aae28eaed4eca04764d511f28992f32b055e8a8
lfabbriend <lucia.fabbri@endava.com>
2022-05-11 15:17:31 -0300

    User welcome view changed 	Continue as guest 	Log in

--- client/js/login.js
+++ client/js/login.js
@@ -1,34 +1,41 @@
 import { baseApiUrl } from './utils/constants.js';
 
 const form = document.getElementById('loginForm');
-const guestBtn = document.getElementById('guestBtn');
+const guestOptionBtn = document.getElementById('guestOptionBtn');
+const userOptionBtn = document.getElementById('userOptionBtn');
 localStorage.setItem('token', undefined);
+const userInput = (document.getElementById('userInput').style.display = 'none');
 
-form.addEventListener('submit', event => {
-	event.preventDefault();
-	const formData = new FormData(event.currentTarget);
-	const userName = formData.get('userName');
-	const password = formData.get('password');
-	fetch(`${baseApiUrl}/login`, {
-		method: 'POST',
-		body: JSON.stringify({
-			userName,
-			password
-		}),
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json'
-		}
-	})
-		.then(response => response.json())
-		.then(json => {
-			localStorage.setItem('token', json[0]);
-		}) //validate that is a tokem, not an error
-		.catch(error => {
-			console.warn(error);
-		});
+userOptionBtn.addEventListener('click', () => {
+	document.getElementById('userInput').style.display = 'block'; //it only appears when page is reloaded
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const userName = formData.get('userName');
+		const password = formData.get('password');
+		fetch(`${baseApiUrl}/login`, {
+			method: 'POST',
+			body: JSON.stringify({
+				userName,
+				password
+			}),
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(response => response.json())
+			.then(json => {
+				localStorage.setItem('token', json[0]);
+				location.href = '../index.html';
+			}) //validate that is a tokem, not an error
+			.catch(error => {
+				console.warn(error);
+			});
+	});
 });
 
-guestBtn.addEventListener('click', () => {
+guestOptionBtn.addEventListener('click', () => {
 	localStorage.setItem('token', undefined);
+	location.href = '../index.html';
 });
