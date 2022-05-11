1f4b9a5bfb7f6ee6dc27071f51b9425d95202f77
lfabbriend <lucia.fabbri@endava.com>
2022-05-11 12:19:46 -0300

    Login token fixed

--- client/js/login.js
+++ client/js/login.js
@@ -2,7 +2,6 @@ import { baseApiUrl } from './utils/constants.js';
 
 const form = document.getElementById('loginForm');
 const guestBtn = document.getElementById('guestBtn');
-localStorage.clear('token');
 localStorage.setItem('token', undefined);
 
 form.addEventListener('submit', event => {
@@ -24,14 +23,12 @@ form.addEventListener('submit', event => {
 		.then(response => response.json())
 		.then(json => {
 			localStorage.setItem('token', json[0]);
-			console.log(localStorage.getItem('token'));
 		}) //validate that is a tokem, not an error
 		.catch(error => {
 			console.warn(error);
-			localStorage.setItem('token', undefined);
 		});
 });
 
 guestBtn.addEventListener('click', () => {
-	localStorage.getItem('token');
+	localStorage.setItem('token', undefined);
 });
