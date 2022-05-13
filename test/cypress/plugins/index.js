e355726c437879919b50c58ea9f96bc52608072c
ENDAVA\JLostumbo <Julian.Lostumbo@endava.com>
2022-05-13 17:14:41 -0300

    cypress.json modified, integration folder created

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const cucumber = require('cypress-cucumber-preprocessor').default
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -17,6 +18,5 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on("file:preprocessor", cucumber())
 }
