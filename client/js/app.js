604a4695437cf53a11a80973e416e46225ae171e
Franco Gaytan <gaytanfranco@gmail.com>
2022-05-13 09:33:00 -0300

    login-logoutBtn

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,13 @@
 import { Rater } from './rater.js';
 import { isLoggedIn } from './utils/helpers.js';
 
+const indexLoginBtn = document.querySelector("#indexLoginBtn");
+
 if (!isLoggedIn()) {
 	document.body.classList.add('guest-user');
+	indexLoginBtn.textContent = "Log in";
+}else{
+	indexLoginBtn.textContent = "Log out";
 }
 
 document.addEventListener('DOMContentLoaded', function () {
