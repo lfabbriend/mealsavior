eb91208835fb1bae5a2666de54789761ab4924ff
ENDAVA\JLostumbo <Julian.Lostumbo@endava.com>
2022-05-17 17:05:55 -0300

    features and steps folders organized, and common.js file added for common steps definitions

--- cypress/support/step_definitions/login/then.js
+++ cypress/support/step_definitions/login/then.js
@@ -1,4 +1,4 @@
-import { Given, When, Then, And, Before } from "cypress-cucumber-preprocessor/steps"
+import {Then, And, Before } from "cypress-cucumber-preprocessor/steps"
 const Login = require("../../page-objects/Login.page")
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -6,50 +6,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // failing the test
     return false
 })
-
-// #region GIVEN Steps
-
-
-Given('I am in the login page', () => {
-    cy.url().should('includes', 'login')
-})
-// #endregion
-
-// #region WHEN Steps
-When('I click the Log In button at the navbar', ()=>{
-    cy.get('.loginBtn').click()
-})
-
-When('I click the Login option in the Welcome section', ()=>{
-    Login.OpenLogin()
-})
-
-When('I click the {string} arrow in the slider', (arrow)=>{
-    if (arrow=="right"){
-        Login.elements.sliderNextArrow().click()
-    }
-    else{
-        Login.elements.sliderPrevArrow().click()
-    }
-})
-
-When('I fill username with {string}', (user)=>{
-    if (user!=""){
-        Login.enterUsername(user)
-    }
-})
-
-
-When('I fill password with {string}', (password)=>{
-    if (password!=""){
-        Login.enterPassword(password)
-    }
-})
-
-When('I click the Login button', ()=>{
-    Login.clickLogInButton()
-})
-// #endregion
 
 // #region THEN Steps
 Then('I should see homepage', ()=>{
