ecf5482e7f663b0df1ee27c83dd6f66b0ac41f77
Franco Gaytan <gaytanfranco@gmail.com>
2022-05-26 16:19:57 -0300

    navbarSizeSearchHidden

--- public/js/utils/app.js
+++ public/js/utils/app.js
@@ -2,12 +2,15 @@ import { Rater } from './rater.js';
 import { isLoggedIn } from './helpers.js';
 
 const indexLoginBtn = document.querySelector("#indexLoginBtn");
+const indexLoginBtnMenu = document.querySelector("#indexLoginBtn2");
 
 if (!isLoggedIn()) {
 	document.body.classList.add('guest-user');
 	indexLoginBtn.textContent = "Log in";
+	indexLoginBtnMenu.textContent = "Log in";
 }else{
 	indexLoginBtn.textContent = "Log out";
+	indexLoginBtnMenu.textContent = "Log out";
 }
 
 document.addEventListener('DOMContentLoaded', function () {
--- public/js/utils/navbar.js
+++ public/js/utils/navbar.js
@@ -4,8 +4,6 @@ menuBtn.addEventListener('click', ()=>{
 	const desplegableMenu = document.querySelector('.desplegableMenu');
 	if(desplegableMenu.classList.contains('show')){
 		desplegableMenu.classList.remove('show');
-		console.log("estoy quitando la clase");
 	}else{
 		desplegableMenu.classList.add('show');
-		console.log("estoy agregando la clase");
 	}});
